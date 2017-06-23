94a9a1fa2df8de8a2f9bc69af6a269786c24c258
Cody Spring <codyspring@me.com>
2017-06-22 22:27:13 -0700

    remove done from sync tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,23 +1,23 @@
 const urlencodedParser = require('../index')
 
 describe('#raw()', () => {
-  it('should parse a single key into itself.', done => {
+  it('should parse a single key into itself.', () => {
     const parsed = urlencodedParser.raw({ 'foo': 1 })
     expect(parsed.foo).toBe(1)
   })
 
-  it('should parse a key with a child into an object.', done => {
+  it('should parse a key with a child into an object.', () => {
     const parsed = urlencodedParser.raw({ 'foo.bar': 1 })
     expect(parsed.foo.bar).toBe(1)
   })
 
-  it('should parse a key with children into an object.', done => {
+  it('should parse a key with children into an object.', () => {
     const parsed = urlencodedParser.raw({ 'foo.bar': 1, 'foo.baz': 2 })
     expect(parsed.foo.bar).toBe(1)
     expect(parsed.foo.baz).toBe(2)
   })
 
-  it('should parse a key with nested children into an object.', done => {
+  it('should parse a key with nested children into an object.', () => {
     const parsed = urlencodedParser.raw({ 'foo.bar.baz': 1 })
     expect(parsed.foo.bar.baz).toBe(1)
   })
