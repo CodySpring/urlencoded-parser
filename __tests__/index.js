39cb2d279f5159972ed9f92e96b9c7b2a4d924e1
Cody Spring <codyspring@me.com>
2017-06-22 21:45:27 -0700

    added tests -- failing

---
+++ __tests__/index.js
@@ -0,0 +1,24 @@
+const urlencodedParser = require('../index')
+
+describe('#raw()', () => {
+  it('should parse a single key into itself.', done => {
+    const parsed = urlencodedParser.raw({ 'foo': 1 })
+    expect(parsed.foo).toBe(1)
+  })
+
+  it('should parse a key with a child into an object.', done => {
+    const parsed = urlencodedParser.raw({ 'foo.bar': 1 })
+    expect(parsed.foo.bar).toBe(1)
+  })
+
+  it('should parse a key with children into an object.', done => {
+    const parsed = urlencodedParser.raw({ 'foo.bar': 1, 'foo.baz': 2 })
+    expect(parsed.foo.bar).toBe(1)
+    expect(parsed.foo.baz).toBe(2)
+  })
+
+  it('should parse a key with nested children into an object.', done => {
+    const parsed = urlencodedParser.raw({ 'foo.bar.baz': 1 })
+    expect(parsed.foo.bar.baz).toBe(1)
+  })
+})
